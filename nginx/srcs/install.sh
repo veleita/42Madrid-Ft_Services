543d3be90b36b9879d88de259366a432b4253fe4
veleita <1veleita1@gmail.com>
2020-09-03 19:09:50 +0200

    Google says: DNS_PROBE_FINISHED_NXDOMAIN

--- nginx/srcs/install.sh
+++ nginx/srcs/install.sh
@@ -5,17 +5,17 @@ apk update
 apk add openssl openssh nginx --no-cache
 
 # Create new user for the web server and add permissions
-adduser -D "__NGINX_USER__"
-mkdir /www
-chown -R "__NGINX_USER__:__NGINX_USER__" /www /var/lib/nginx
+adduser -D admin
+mkdir -p /var/www /run/nginx
+chown -R admin:admin /var/www /run/nginx /var/lib/nginx
+chmod 755 /var/www /run/nginx /var/lib/nginx
+
+# Request SSL key
+openssl req -x509 -newkey rsa:2048 -nodes -subj "/C=SP/L=Madrid/O=42" -keyout /etc/ssl/private/admin.key -out /etc/ssl/certs/admin.crt
 
 # Create a new SSH user and password
-adduser -D "__SSH_USER__"
-echo "__SSH_USER__:__SSH_PASSWORD__" | chpasswd
+adduser -D mzomeno-
+echo mzomeno-:psswd | chpasswd
 
 # Generate SSH key
 ssh-keygen -A
-
-# Request SSL key
-openssl req -newkey rsa:2048 -nodes -subj "/C=SP/L=Madrid/O=42" \
--keyout /etc/ssl/private/admin.key -out /etc/ssl/certs/admin.crt
