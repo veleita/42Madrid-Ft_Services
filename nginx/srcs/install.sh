0d5c3ffa7ade4d7eb454f13fcbea6cf5bdd7760b
veleita <1veleita1@gmail.com>
2020-09-10 19:53:18 +0200

    ftps server configured but not tested, arranged some minor issues in the nginx server, beware the unset variables

--- nginx/srcs/install.sh
+++ nginx/srcs/install.sh
@@ -5,17 +5,20 @@ apk update
 apk add openssl openssh nginx --no-cache
 
 # Create new user for the web server and add permissions
-adduser -D admin
+adduser -D "$NGINX_USER"
 mkdir -p /var/www /run/nginx
-chown -R admin:admin /var/www /run/nginx /var/lib/nginx
+chown -R "$NGINX_USER":"$NGINX_USER" /var/www /run/nginx /var/lib/nginx
 chmod 755 /var/www /run/nginx /var/lib/nginx
 
 # Request SSL key
-openssl req -x509 -newkey rsa:2048 -nodes -subj "/C=SP/L=Madrid/O=42" -keyout /etc/ssl/private/admin.key -out /etc/ssl/certs/admin.crt
+openssl req -x509 -newkey rsa:2048 -nodes \
+-subj "/C=${COUNTRY}/L=${CITY}/O=${ORGANIZATION}" \
+-keyout /etc/ssl/private/${NGINX_USER}.key \
+-out /etc/ssl/certs/${NGINX_USER}.crt
 
 # Create a new SSH user and password
-adduser -D mzomeno-
-echo mzomeno-:psswd | chpasswd
+adduser -D "$SSH_USER"
+echo "$SSH_USER":"$SSH_PASSWORD" | chpasswd
 
 # Generate SSH key
 ssh-keygen -A
