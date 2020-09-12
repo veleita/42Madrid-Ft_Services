b75ddb32021b8286917c8c126692ab8b3209a508
veleita <1veleita1@gmail.com>
2020-09-12 20:18:45 +0200

    Temporarily erased variables and changed some of the configuration settings including the ports for the passive connection

--- ftps/srcs/install.sh
+++ ftps/srcs/install.sh
@@ -5,29 +5,33 @@ apk update
 apk add openssl vsftpd
 
 # Create new user for the server
-adduser -D "$FTPS_USER"
-echo "$FTPS_USER":"$FTPS_PASSWORD" | chpasswd
+adduser -D mzomeno-
+echo mzomeno-:ftpsswrd | chpasswd
+echo mzomeno- >> /etc/vsftpd.chroot_list
 
 # Request SSL key
 openssl req -x509 -newkey rsa:2048 -nodes \
--subj "/C=${COUNTRY}/L=${CITY}/O=${ORGANIZATION}" \
--keyout /etc/ssl/private/${FTPS_USER}.key \
--out /etc/ssl/certs/${FTPS_USER}.crt
+-subj "/C=SP/L=Madrid/O=42" \
+-keyout /etc/ssl/private/mzomeno-.key \
+-out /etc/ssl/certs/mzomeno-.crt
 
 # Generate configuration file
 mkdir -p /etc/vsftpd
 cat <<EOF >> /etc/vsftpd/vsftpd.conf
 seccomp_sandbox=NO
-pasv_min_port=30020
-pasv_max_port=30021
-pasv_address=192.168.99.104
+pasv_enable=YES
+pasv_min_port=2121
+pasv_max_port=2122
+pasv_address=0.0.0.0
 local_enable=YES
 write_enable=YES
+chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd.chroot_list
 local_umask=022
 dirmessage_enable=YES
 xferlog_enable=YES
 connect_from_port_20=YES
 ssl_enable=YES
-rsa_cert_file=/etc/ssl/certs/${FTPS_USER}.crt
-rsa_private_key_file=/etc/ssl/private/${FTPS_USER}.key
+rsa_cert_file=/etc/ssl/certs/mzomeno-.crt
+rsa_private_key_file=/etc/ssl/private/mzomeno-.key
 EOF
